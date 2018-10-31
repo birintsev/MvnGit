7259d893e57a2d90ecfec27b6f969a2cce07c919
leader228228 <leader228228@gmail.com>
2018-10-31 23:06:03 +0200

    Initial commit

---
+++ src/main/java/ua/sumdu/j2se/Birintsev/App.java
@@ -0,0 +1,13 @@
+package ua.sumdu.j2se.Birintsev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ua/sumdu/j2se/Birintsev/AppTest.java
@@ -0,0 +1,20 @@
+package ua.sumdu.j2se.Birintsev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
